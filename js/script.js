5e57277fe0050a40eb4d24aa0ea54332e7e6246f
joiilah7 <joiilahh@gmail.com>
2020-01-14 02:21:43 +0000

    Finished day one, started on day 2

---
+++ js/script.js
@@ -0,0 +1,52 @@
+
+//DAY ONE: 
+
+let songNameArray = ["test"];
+
+songNameArray.forEach(function(song){
+  
+    $(".songDisplay").append('<li>'+ song +'</li>');
+
+});
+
+let songArtistArray = ["test2"];
+
+songArtistArray.forEach(function(artist){
+  
+    $(".songDisplay").append('<li>'+ artist +'</li>');
+
+});
+
+let songLengthArray = ["test3"];
+
+songLengthArray.forEach(function(songLength){
+  
+    $(".songDisplay").append('<li>'+ songLength +'</li>');
+
+});
+
+let songImageArray = ["test4"];
+
+songImageArray.forEach(function(image){
+  
+    $(".songDisplay").append('<li>'+ image +'</li>');
+
+});
+
+let songLinkArray = ["test5"];
+
+songLinkArray.forEach(function(link){
+  
+    $(".songDisplay").append('<li>'+ link +'</li>');
+
+});
+
+// DAY TWO: 
+ let newName = $(".songName") .val();
+ let newArtist = $(".songArtist").val();
+ let newLength = $(".songLength").val();
+ let newImage = $(".songImage").val();
+ let newLink = $(".songLink").val();
+
+ songNameArray.push(newName);
+ songArtistArray.push(newArtist)
